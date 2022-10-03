ff73722e94d27adc8cb6be2cb0282d92839f57f3
Marsel <marchello.marsel@gmail.com>
2022-10-03 15:04:19 +0600

    добавил динамическое отображение списка из БД через свойства компонентов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,14 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+	const data = [
+		{ name: 'John Smith', salary: 1500, increase: false, id: 1},
+		{ name: 'Alex Levin', salary: 1000, increase: false, id: 2},
+		{ name: 'Bron Kann', salary: 800, increase: true, id: 3},
+		{ name: 'Linda Forest', salary: 1200, increase: false, id: 4},
+		{ name: 'Mary Morgan', salary: 900, increase: true, id: 5},
+	];
+
 	return (
 		<div className='app'>
 			<AppInfo />
@@ -16,7 +24,7 @@ function App() {
 				<AppFilter />
 			</div>
 
-			<EmployersList />
+			<EmployersList data={data} />
 
 			<EmployersAddForm />
 		</div>
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -2,14 +2,13 @@ import EmployersListItem from '../employers-list-item/employers-list-item';
 
 import './employers-list.css';
 
-const EmployersList = () => {
-	return (
-		<ul className='app-list list-group'>
-			<EmployersListItem />
-			<EmployersListItem />
-			<EmployersListItem />
-		</ul>
-	);
+const EmployersList = ({ data }) => {
+	const elements = data.map((item) => {
+		const { id, ...itemProps } = item;
+		return <EmployersListItem key={id} {...itemProps} />;
+	});
+
+	return <ul className='app-list list-group'>{elements}</ul>;
 };
 
 export default EmployersList;
