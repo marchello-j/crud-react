6131c1ff9badd0f9719d2b19ebbb07cf46c3aeb7
Marsel <marchello.marsel@gmail.com>
2022-10-05 16:48:51 +0600

    добавлен функционал добавления и удаления сотрудника в список

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,29 +7,61 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
-	const data = [
-		{ name: 'John Smith', salary: 1500, increase: false, id: 1},
-		{ name: 'Alex Levin', salary: 1000, increase: false, id: 2},
-		{ name: 'Bron Kann', salary: 800, increase: true, id: 3},
-		{ name: 'Linda Forest', salary: 1200, increase: false, id: 4},
-		{ name: 'Mary Morgan', salary: 900, increase: true, id: 5},
-	];
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ name: 'John Smith', salary: 1500, increase: false, id: 1 },
+				{ name: 'Alex Levin', salary: 1000, increase: false, id: 2 },
+				{ name: 'Bron Kann', salary: 800, increase: true, id: 3 },
+				{ name: 'Linda Forest', salary: 1200, increase: false, id: 4 },
+				{ name: 'Mary Morgan', salary: 900, increase: true, id: 5 },
+			],
+		};
+		this.maxId = 6;
+	}
 
-	return (
-		<div className='app'>
-			<AppInfo />
+	deleteItem = (id) => {
+		this.setState(({ data }) => {
+			return {
+				data: data.filter((elem) => elem.id !== id),
+			};
+		});
+	};
 
-			<div className='search-panel'>
-				<SearchPanel />
-				<AppFilter />
+	addItem = (name, salary) => {
+		const newItem = {
+			name: name,
+			salary: salary,
+			increase: false,
+			id: this.maxId++,
+		};
+		this.setState(({ data }) => {
+			const newArr = [...data, newItem];
+			return {
+				data: newArr,
+			};
+		});
+	};
+
+	render() {
+		const { data } = this.state;
+		return (
+			<div className='app'>
+				<AppInfo />
+
+				<div className='search-panel'>
+					<SearchPanel />
+					<AppFilter />
+				</div>
+
+				<EmployersList data={data} onDelete={this.deleteItem} />
+
+				<EmployersAddForm onAdd={this.addItem} />
 			</div>
-
-			<EmployersList data={data} />
-
-			<EmployersAddForm />
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -22,7 +22,8 @@ class EmployersListItem extends Component {
 		}));
 	};
 	render() {
-		const { name, salary } = this.props;
+		const { name, salary, onDelete
+		 } = this.props;
 		const { increase, rise } = this.state;
 		let classNames = 'list-group-item d-flex justify-content-between';
 		if (increase) {
@@ -52,7 +53,11 @@ class EmployersListItem extends Component {
 						<i className='fas fa-cookie'></i>
 					</button>
 
-					<button type='button' className='btn-trash btn-sm '>
+					<button
+						type='button'
+						className='btn-trash btn-sm '
+						onClick={onDelete}
+					>
 						<i className='fas fa-trash'></i>
 					</button>
 					<i className='fas fa-star'></i>
